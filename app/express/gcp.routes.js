871f81b037ceb22ca169e8afb769ca84505130d1
rob <rca06d@my.fsu.edu>
2019-04-14 17:31:50 -0600

    fix sign url route

--- app/express/gcp.routes.js
+++ app/express/gcp.routes.js
@@ -2,7 +2,13 @@ const gcsService = require('../gcs.service');
 
 module.exports = app => {
 	app.get('/upload', async (req, res) => {
-		const filename = Date.now();
-		res.json({ filename, url: await gcsService.getSignedUrl(filename) });
+		const filename = Date.now() + '';
+		try {
+			const url = await gcsService.getSignedUrl(filename);
+			res.json({ filename, url });
+		} catch (e) {
+			console.error('dingus', e);
+			res.status(500).send(e);
+		}
 	});
 };
