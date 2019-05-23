9c41f0ab5265139172d68169a2569836b243817e
rob <rca06d@my.fsu.edu>
2019-05-23 15:11:01 -0600

    get time annotations and punctuation

--- app/gcs.service.js
+++ app/gcs.service.js
@@ -27,7 +27,9 @@ async function initSpeechToTextOp(filename, options = {}) {
 		config: {
 			encoding: 'LINEAR16', // LINEAR16 is PCM at bit depth 16, https://cloud.google.com/speech-to-text/docs/encoding
 			// sampleRateHertz: 44100, // omit to automatically set
-			languageCode: 'en-US',
+			languageCode: 'en-GB', // en-US (american english), en-GB (british english), ...https://cloud.google.com/speech-to-text/docs/languages
+			enableWordTimeOffsets: true,
+			enableAutomaticPunctuation: true,
 			...options,
 		},
 		audio: {
