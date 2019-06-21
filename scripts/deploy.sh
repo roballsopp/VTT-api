2d1d12dd7e22c039c4c7e959c1d24bfb00a1deda
rob <rca06d@my.fsu.edu>
2019-06-21 17:53:19 -0600

    hopefully load env vars from actual file

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,4 +3,4 @@ cd /var/www/api.vtt-creator.com
 git reset --hard
 git fetch --tags "https://$USERNAME:$PASSWORD@github.com/roballsopp/vtt-creator-backend.git"
 git checkout $TAG
-pm2 start app/index.js --name api.vtt-creator.com
+pm2 start app/index.js --name api.vtt-creator.com --node-args="-r dotenv/config"
