695e57d97b36c8c3502e2d2e13c5b044c4222c91
rob <rca06d@my.fsu.edu>
2019-06-21 23:48:31 -0600

    maybe this?

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,4 +3,5 @@ cd /var/www/api.vtt-creator.com
 git reset --hard
 git fetch --tags "https://$USERNAME:$PASSWORD@github.com/roballsopp/vtt-creator-backend.git"
 git checkout $TAG
-pm2 restart app --name api.vtt-creator.com --node-args="-r dotenv/config"
+pm2 stop api.vtt-creator.com
+pm2 start app --name api.vtt-creator.com --node-args="-r dotenv/config"
