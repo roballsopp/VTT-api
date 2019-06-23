4fd5824d8394d34e7d46786cd26e089cac1b62de
rob <rca06d@my.fsu.edu>
2019-06-23 12:45:54 -0600

    eek definitely need to yarn install

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,5 +3,6 @@ cd /var/www/api.vtt-creator.com
 git reset --hard
 git fetch --tags "https://$USERNAME:$PASSWORD@github.com/roballsopp/vtt-creator-backend.git"
 git checkout $TAG
+yarn
 pm2 stop api.vtt-creator.com
 pm2 start app --name api.vtt-creator.com --node-args="-r dotenv/config"
