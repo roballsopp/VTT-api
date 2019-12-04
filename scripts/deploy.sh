d814cf45128bf3a2ae5bec5ea8b8667ade6e3fdd
rob <rca06d@my.fsu.edu>
2019-12-04 10:17:51 -0700

    dont need this script anymore

--- scripts/deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -e
-cd /var/www/api.vtt-creator.com
-git reset --hard
-git fetch --tags "https://$USERNAME:$PASSWORD@github.com/roballsopp/vtt-creator-backend.git"
-git checkout $TAG
-yarn
-pm2 stop api.vtt-creator.com
-pm2 start app --name api.vtt-creator.com --node-args="-r dotenv/config"
