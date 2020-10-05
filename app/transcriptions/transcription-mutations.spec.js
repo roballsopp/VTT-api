fde57f482cd6b0fdf160692f87ee47056dd78eda
rob <rca06d@my.fsu.edu>
2020-10-05 08:51:49 -0600

    finish test for unlimited usage accounts too

--- app/transcriptions/transcription-mutations.spec.js
+++ app/transcriptions/transcription-mutations.spec.js
@@ -194,16 +194,17 @@ describe('beginTranscription mutation', function() {
 			this.expectedDuration = 20;
 			this.expectedFilename = 'neat_file_in_bucket';
 
-			const mockGcpModel = {
+			this.mockGcpModel = {
 				getAudioInfo: () => ({ duration: this.expectedDuration }),
 				initSpeechToTextOp: () => this.expectedOpId,
+				deleteFile: spy(() => Promise.resolve()),
 			};
 
 			await this.updateTestUser({ credit: '0', unlimited_usage: '1' });
 
-			const server = this.createServer({ gcp: mockGcpModel });
+			this.server = this.createServer({ gcp: this.mockGcpModel });
 
-			const { statusCode, body } = await request(server)
+			const { statusCode, body } = await request(this.server)
 				.post('/graphql')
 				.send({
 					query: `mutation beginTranscription($filename: String!, $languageCode: String!) {
@@ -228,35 +229,98 @@ describe('beginTranscription mutation', function() {
 
 			if (statusCode !== 200) throw new Error(`Error starting transcription: ${body.errors}`);
 
-			this.result = body.data.beginTranscription;
+			this.beginResult = body.data.beginTranscription;
 		});
 
 		it('returns a job with an id', async function() {
-			assert.isOk(this.result.job.id);
+			assert.isOk(this.beginResult.job.id);
 		});
 
 		it('returns the expected file key', async function() {
-			expect(this.result.job.fileKey).to.equal(this.expectedFilename);
+			expect(this.beginResult.job.fileKey).to.equal(this.expectedFilename);
 		});
 
 		it('calculates the correct cost', async function() {
-			expect(this.result.job.cost).to.equal(Math.ceil((this.expectedDuration / 60) * 0.15 * 100) / 100);
+			expect(this.beginResult.job.cost).to.equal(Math.ceil((this.expectedDuration / 60) * 0.15 * 100) / 100);
 		});
 
 		it('sets the job to a `pending` state', async function() {
-			expect(this.result.job.state).to.equal('pending');
+			expect(this.beginResult.job.state).to.equal('pending');
 		});
 
 		it('returns an operation id', async function() {
-			expect(this.result.job.operationId).to.equal(this.expectedOpId);
+			expect(this.beginResult.job.operationId).to.equal(this.expectedOpId);
 		});
 
 		it('returns the correct user', async function() {
-			expect(this.result.job.user.id).to.equal(this.testUserId);
+			expect(this.beginResult.job.user.id).to.equal(this.testUserId);
 		});
 
 		it('does not charge the user', async function() {
-			expect(this.result.job.user.credit).to.equal(0);
+			expect(this.beginResult.job.user.credit).to.equal(0);
+		});
+
+		describe('then, when the user finishes the transcription', function() {
+			before(async function() {
+				const { statusCode, body } = await request(this.server)
+					.post('/graphql')
+					.send({
+						query: `mutation finishTranscription($operationId: String!) {
+							finishTranscription(operationId: $operationId) {
+								job {
+									id
+									fileKey
+									cost
+									createdAt
+									operationId
+									state
+									user {
+										id
+										credit
+									}
+								}
+							}
+						}`,
+						variables: { operationId: this.expectedOpId },
+					})
+					.set('Authorization', `Bearer ${this.testUserToken}`);
+
+				if (statusCode !== 200) throw new Error(`Error finishing transcription: ${body.errors}`);
+
+				this.finishResult = body.data.finishTranscription;
+			});
+
+			it('returns the same job id as before', async function() {
+				expect(this.finishResult.job.id).to.equal(this.beginResult.job.id);
+			});
+
+			it('returns the same file key as before', async function() {
+				expect(this.finishResult.job.fileKey).to.equal(this.expectedFilename);
+			});
+
+			it('returns the correct cost', async function() {
+				expect(this.finishResult.job.cost).to.equal(0.05);
+			});
+
+			it('returns the correct user', async function() {
+				expect(this.finishResult.job.user.id).to.equal(this.testUserId);
+			});
+
+			it('still does not charge the user', async function() {
+				expect(this.finishResult.job.user.credit).to.equal(0);
+			});
+
+			it('sets the job to a `success` state', async function() {
+				expect(this.finishResult.job.state).to.equal('success');
+			});
+
+			it('returns the previous operation id', async function() {
+				expect(this.finishResult.job.operationId).to.equal(this.expectedOpId);
+			});
+
+			it('cleans up the transcription file when done', async function() {
+				expect(this.mockGcpModel.deleteFile).to.have.been.called.with(this.expectedFilename);
+			});
 		});
 	});
 });
